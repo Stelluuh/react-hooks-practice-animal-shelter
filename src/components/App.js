72e3126551282554967cbf752f8136e180a35288
stella <sftamesis13@gmail.com>
2022-11-20 17:46:19 -0800

    complete all except filter based on type = "all"

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,27 @@ function App() {
   }
 
   function findPets() {
-    console.log('find the pets!')
     
     fetch(`http://localhost:3001/pets/`)
       .then(response => response.json())
       .then(pets => { 
-        const petsArray = pets.filter( pet => pet.type === filters)
-        console.log(petsArray)
+        
+        const petsArray = pets.filter( pet => pet.type === filters )
+        // console.log(petsArray)
+        
         setPets(petsArray)
       }
     )
   }
+
+  function adoptPet(petId) {
+    const updatedPet = pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted : true } : pet;
+    })
+
+    setPets(updatedPet)
+  }
+
 
   return (
     <div className="ui container">
@@ -36,7 +46,7 @@ function App() {
             <Filters onChangeType={selectAnimalType} onFindPetsClick={findPets}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={adoptPet}/>
           </div>
         </div>
       </div>
